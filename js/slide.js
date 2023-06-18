2a11d8c93146704496833e8589be48dea999e4bc
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-18 16:08:20 -0300

    Métodos iniciais e dando init no slide

--- js/slide.js
+++ js/slide.js
@@ -3,4 +3,17 @@ export default class Slide {
     this.wrapper = document.querySelector(wrapper);
     this.slide = document.querySelector(slide);
   }
+
+  onStart(event) {
+    event.preventDefault();
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener('mousedown', () => this.onStart);
+  }
+
+  init() {
+    this.addSlideEvents();
+    return this;
+  }
 }
