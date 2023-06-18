1a9ced3a7e71df7565fee3d76aa4c9389c835f05
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-18 16:17:16 -0300

    Funcionalidades padrão

--- js/slide.js
+++ js/slide.js
@@ -6,19 +6,29 @@ export default class Slide {
 
   onStart(event) {
     event.preventDefault();
+    this.wrapper.addEventListener('mousemove', this.onMove);
   }
+
+  onEnd(event) {
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+  }
+
+  onMove(event) {}
 
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
   }
 
   bindEvents() {
     this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
   }
 
   init() {
-    this.addSlideEvents();
     this.bindEvents();
+    this.addSlideEvents();
     return this;
   }
 }
