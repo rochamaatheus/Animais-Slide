ebdecf601fc10399f869595eefc9ba7209cab6ee
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-19 19:11:45 -0300

    Documentação do Slide

--- js/slide.js
+++ js/slide.js
@@ -1,28 +1,61 @@
 import debounce from './debounce.js';
 
+/**
+ * Classe Slide para criar um slide funcional.
+ * @class
+ * @export
+ */
 export class Slide {
+  /**
+   * Classe responsável por criar um Slide.
+   * @param {string} wrapper - Classe CSS para o elemento wrapper do Slide.
+   * @param {string} slide - Classe CSS para o elemento slide.
+   * @param {string} activeClass - Classe CSS para o slide ativo.
+   */
   constructor(wrapper, slide, activeClass) {
+    /** @type {Element} */
     this.wrapper = document.querySelector(wrapper);
+    /** @type {Element} */
     this.slide = document.querySelector(slide);
+    /** @type {string} */
     this.activeClass = activeClass;
+    /** @type {{ finalPosition: number, startX: number, movement: number, movePosition?: number }} */
     this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+    /** @type {Event} */
     this.changeEvent = new Event('changeEvent');
   }
 
+  /**
+   * Adiciona a transição do slide.
+   * @param {boolean} active - Indica se a transição está ativa.
+   */
   transition(active) {
     this.slide.style.transition = active ? 'transform .3s' : '';
   }
 
+  /**
+   * Move o slide para uma determinada posição horizontal.
+   * @param {number} distX - Posição horizontal do slide.
+   */
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
   }
 
+  /**
+   * Atualiza a posição do slide com base na posição atual do ponteiro.
+   * @param {number} clientX - Posição horizontal do ponteiro.
+   * @returns {number} - Nova posição final do slide.
+   */
   updatePosition(clientX) {
     this.dist.movement = (this.dist.startX - clientX) * 1.8;
     return this.dist.finalPosition - this.dist.movement;
   }
 
+  /**
+   * Manipulador do evento de início do movimento do slide.
+   * @param {MouseEvent | TouchEvent} event - Evento de início do movimento.
+   */
   onStart(event) {
     let movetype;
     if (event.type === 'mousedown') {
@@ -37,6 +70,10 @@ export class Slide {
     this.transition(false);
   }
 
+  /**
+   * Manipulador do evento de finalização do movimento do slide.
+   * @param {MouseEvent | TouchEvent} event - Evento de finalização do movimento.
+   */
   onEnd(event) {
     const movetype = event.type === 'mouseup' ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(movetype, this.onMove);
@@ -45,6 +82,10 @@ export class Slide {
     this.changeSlideOnEnd();
   }
 
+  /**
+   * Manipulador do evento de movimento do slide.
+   * @param {MouseEvent | TouchEvent} event - Evento de movimento do slide.
+   */
   onMove(event) {
     const pointerPosition =
       event.type === 'mousemove'
@@ -54,6 +95,9 @@ export class Slide {
     this.moveSlide(finalPosition);
   }
 
+  /**
+   * Verifica a mudança de slide após o término do movimento.
+   */
   changeSlideOnEnd() {
     if (this.dist.movement > 120 && this.index.next !== undefined) {
       this.activeNextSlide();
@@ -64,6 +108,9 @@ export class Slide {
     }
   }
 
+  /**
+   * Adiciona os eventos de interação do slide.
+   */
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
     this.wrapper.addEventListener('touchstart', this.onStart);
@@ -71,6 +118,9 @@ export class Slide {
     this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
+  /**
+   * Liga os eventos aos métodos correspondentes.
+   */
   bindEvents() {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
@@ -82,11 +132,19 @@ export class Slide {
 
   // Slides config
 
+  /**
+   * Calcula a posição do slide com base na margem.
+   * @param {Element} slide - Elemento do slide.
+   * @returns {number} - Posição do slide.
+   */
   slidePosition(slide) {
     const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
     return -(slide.offsetLeft - margin);
   }
 
+  /**
+   * Configuração dos slides.
+   */
   slidesConfig() {
     this.slideArray = [...this.slide.children].map((element) => {
       const position = this.slidePosition(element);
@@ -97,6 +155,10 @@ export class Slide {
     });
   }
 
+  /**
+   * Configuração dos índices da navegação do slide.
+   * @param {number} index - Índice do slide ativo.
+   */
   slidesIndexNav(index) {
     const last = this.slideArray.length - 1;
     this.index = {
@@ -106,6 +168,10 @@ export class Slide {
     };
   }
 
+  /**
+   * Muda para um determinado slide.
+   * @param {number} index - Índice do slide.
+   */
   changeSlide(index) {
     const activeSlide = this.slideArray[index];
     this.moveSlide(activeSlide.position);
@@ -115,6 +181,10 @@ export class Slide {
     this.wrapper.dispatchEvent(this.changeEvent);
   }
 
+  /**
+   * Remove a classe CSS dos slides ativo existentes.
+   * E adiciona a classe ativa apenas para o elemento selecionado.
+   */
   changeActiveClass() {
     this.slideArray.forEach((item) =>
       item.element.classList.remove(this.activeClass)
@@ -122,14 +192,23 @@ export class Slide {
     this.slideArray[this.index.active].element.classList.add(this.activeClass);
   }
 
+  /**
+   * Ativa o slide anterior.
+   */
   activePrevSlide() {
     if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
   }
 
+  /**
+   * Ativa o próximo slide.
+   */
   activeNextSlide() {
     if (this.index.next !== undefined) this.changeSlide(this.index.next);
   }
 
+  /**
+   * Manipulador do evento de redimensionamento da janela.
+   */
   onResize() {
     setTimeout(() => {
       this.slidesConfig();
@@ -137,10 +216,17 @@ export class Slide {
     }, 200);
   }
 
+  /**
+   * Adiciona o evento de redimensionamento da janela.
+   */
   addResizeEvent() {
     window.addEventListener('resize', this.onResize);
   }
 
+  /**
+   * Inicializa o Slide.
+   * @returns {Slide} - Instância do Slide.
+   */
   init() {
     this.transition(true);
     this.bindEvents();
@@ -153,22 +239,40 @@ export class Slide {
 }
 
 export class SlideNav extends Slide {
+  /**
+   * Classe responsável por criar um Slide com navegação.
+   * @param {...any} args - Argumentos do construtor da classe Slide.
+   */
   constructor(...args) {
     super(...args);
     this.bindControlEvent();
   }
 
+  /**
+   * Adiciona as setas de navegação do slide.
+   * @param {string} prev - Seletor do elemento de navegação para slide anterior.
+   * @param {string} next - Seletor do elemento de navegação para próximo slide.
+   */
   addArrow(prev, next) {
+    /** @type {Element} */
     this.prevElement = document.querySelector(prev);
+    /** @type {Element} */
     this.nextElement = document.querySelector(next);
     this.addArrowEvent();
   }
 
+  /**
+   * Adiciona os eventos de clique nas setas de navegação.
+   */
   addArrowEvent() {
     this.prevElement.addEventListener('click', this.activePrevSlide);
     this.nextElement.addEventListener('click', this.activeNextSlide);
   }
 
+  /**
+   * Cria a navegação por controle.
+   * @returns {Element} - Elemento de controle criado.
+   */
   createControl() {
     const control = document.createElement('ul');
     control.dataset.control = 'slide';
@@ -181,6 +285,11 @@ export class SlideNav extends Slide {
     return control;
   }
 
+  /**
+   * Manipulador do evento de controle de clique.
+   * @param {Element} item - Item do controle.
+   * @param {number} index - Índice do slide correspondente ao item do controle.
+   */
   eventControl(item, index) {
     item.addEventListener('click', (event) => {
       event.preventDefault();
@@ -189,6 +298,9 @@ export class SlideNav extends Slide {
     this.wrapper.addEventListener('changeEvent', this.activeControlItem);
   }
 
+  /**
+   * Ativa o item de controle correspondente ao slide ativo.
+   */
   activeControlItem() {
     this.controlArray.forEach((item) =>
       item.classList.remove(this.activeClass)
@@ -196,6 +308,10 @@ export class SlideNav extends Slide {
     this.controlArray[this.index.active].classList.add(this.activeClass);
   }
 
+  /**
+   * Adiciona a navegação por controle.
+   * @param {string} customControl - Seletor do elemento de controle personalizado (opcional).
+   */
   addControl(customControl) {
     this.control =
       document.querySelector(customControl) || this.createControl();
@@ -204,6 +320,9 @@ export class SlideNav extends Slide {
     this.controlArray.forEach(this.eventControl);
   }
 
+  /**
+   * Liga os eventos de controle aos métodos correspondentes.
+   */
   bindControlEvent() {
     this.eventControl = this.eventControl.bind(this);
     this.activeControlItem = this.activeControlItem.bind(this);
